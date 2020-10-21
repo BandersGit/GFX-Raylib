92f4184e86b3093baf4ba4603b3e0925fc017785
BandersGit <andy.i.flaskan@gmail.com>
2020-10-21 11:06:26 +0200

    Xwing png and initial commit

---
+++ GFX/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Grafik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(1800, 1000, "Hello TE!");
+
+            Image xWing = Raylib.LoadImage(@"XWing.png");
+            Texture2D xWingTexture = Raylib.LoadTextureFromImage(xWing);
+
+            float x = 0;
+            float y = 0;
+
+            while (!Raylib.WindowShouldClose())
+            {
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+                {
+                    x+= 1f;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+                {
+                    x-= 1f;
+                }
+
+                if(Raylib.IsKeyDown(KeyboardKey.KEY_S))
+                {
+                    y+= 1f;
+                }
+
+                if(Raylib.IsKeyDown(KeyboardKey.KEY_W))
+                {
+                    y-= 1f;
+                }
+
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(Color.BLACK);
+
+                Raylib.DrawCircle(400, 300, 100, Color.GRAY);
+                Raylib.DrawCircle(400, 300, 50, Color.DARKGRAY);
+
+                
+                Raylib.DrawTexture(xWingTexture, (int) x, (int) y, Color.WHITE);
+                Raylib.EndDrawing();
+
+            }
+        }
+    }
+}
